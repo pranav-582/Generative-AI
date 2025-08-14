2c9dbab3476cd126bae12ae1b4d2cc39b534d58d
Pranav <kumarpranav582@gmail.com>
2025-08-14 18:54:29 +0530

    Explored different ConversationBufferWindowMemory, ConversationSummaryMemory, and CombinedMemory in the ConversationChain

---
+++ langchain/conversations_chains.py
@@ -0,0 +1,39 @@
+from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.memory import ConversationSummaryMemory
+from langchain.memory import CombinedMemory
+from langchain.chains import ConversationChain
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+
+# memory = ConversationBufferMemory(return_messages=True)
+# memory = ConversationBufferWindowMemory(k=3, return_messages=True)
+# memory = ConversationSummaryMemory(llm=llm, return_messages=True)
+memory = CombinedMemory(
+    memories=[
+        ConversationBufferMemory(return_messages=True),
+        ConversationSummaryMemory(llm=llm, return_messages=True)
+    ]
+)
+
+
+# ConversationChain
+conversation = ConversationChain(
+    llm=llm,
+    memory=memory,
+    verbose=True
+)
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit"]:
+        print("Goodbye!")
+        break
+    response = conversation.predict(input=user_input)
+    print("Assistant:", response)
+
